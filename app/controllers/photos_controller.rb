e9015c2d9190fb43333182beacb7ac004dfcdd26
atrivedi22 <atrivedi2016@kellogg.northwestern.edu>
2015-05-08 08:29:06 -0500

    finished hw
    5.8.15

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,39 @@ class PhotosController < ApplicationController
   end
 
 
+  def show
+    @photo = Photo.find(params[:id])
+  end
+
+def new_form
+  end
+
+  def create_row
+  p = Photo.new
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
+
+  redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+       p = Photo.find(params[:id])
+    p.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+def edit_form
+ @photo = Photo.find(params[:id])
+  end
+
+  def update_row
+     p = Photo.new
+    p.source = params[:the_source]
+    p.caption = params[:the_caption]
+    p.save
+        redirect_to("http://localhost:3000/photos/#{p.id}")
+      end
+
 end
